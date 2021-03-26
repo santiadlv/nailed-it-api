9c08b8f5473fa86b8ec1f7610f506b620079a7c2
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-03-26 16:10:11 -0700

    Developed unit tests for the forget password function

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,11 +1,14 @@
 import pytest
-import mongomock
 from ..main import app
+from ..core import settings
 from fastapi.testclient import TestClient
+from motor.motor_asyncio import AsyncIOMotorClient
 
 @pytest.fixture(scope="module")
 def test_app():
-    app.mongodb_client = mongomock.MongoClient()
-    app.mongodb = app.mongodb_client.db
+    app.mongodb_client = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.mongodb = app.mongodb_client[settings.MONGODB_NAME]
     client = TestClient(app)
+    client.mongodb_client = AsyncIOMotorClient(settings.MONGODB_URL)
+    client.mongodb = app.mongodb_client[settings.MONGODB_NAME]
     yield client
---
+++ api/tests/test_forget.py
@@ -0,0 +1,66 @@
+
+def test_forget_get_token(test_app):
+    test_user = {
+        "email": "forget@pwd.com",
+    }
+
+    response = test_app.post("/users/reset", json=test_user)
+    assert response.status_code == 202
+    assert response.json().get('message') == "Token generated successfully"
+
+def test_forget_user_not_found(test_app):
+    test_user = {
+        "email": "nonexistent@user.com",
+    }
+
+    response = test_app.post("/users/reset", json=test_user)
+    assert response.status_code == 404
+    assert response.json().get('detail') == f"User with email {test_user['email']} not found"
+
+def test_forget_bad_email(test_app):
+    test_user = {
+        "email": "nonexistent@",
+    }
+
+    response = test_app.post("/users/reset", json=test_user)
+    assert response.status_code == 422
+    assert response.json().get('detail')[0].get('msg') == "value is not a valid email address"
+
+def test_forget_pwd_updated(test_app):
+    test_user = {
+        "token": "605e67283f5b377dfe2c1143",
+        "new_password": "NewTestPassword1."
+    }
+
+    response = test_app.post("/users/reset/token", json=test_user)
+    assert response.status_code == 200
+    assert response.json().get('message') == "Resource updated successfully."
+
+def test_forget_bad_token(test_app):
+    test_user = {
+        "token": "NotAToken",
+        "new_password": "NewTestPassword1."
+    }
+
+    response = test_app.post("/users/reset/token", json=test_user)
+    assert response.status_code == 404
+    assert response.json().get('detail') == "Could not change password using provided token"
+
+def test_forget_weak_pwd(test_app):
+    test_user = {
+        "token": "605e67283f5b377dfe2c1143",
+        "new_password": "weak"
+    }
+
+    response = test_app.post("/users/reset/token", json=test_user)
+    assert response.status_code == 422
+    assert response.json().get('detail')[0].get('msg') == "Password must be 8 characters or longer"
+
+def test_forget_missing_field(test_app):
+    test_user = {
+        "token": "605e67283f5b377dfe2c1143"
+    }
+
+    response = test_app.post("/users/reset/token", json=test_user)
+    assert response.status_code == 422
+    assert response.json().get('detail')[0].get('msg') == "field required"

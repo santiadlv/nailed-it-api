503fa53cf4077514595d669ac29f9417561210dd
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-03-26 01:59:33 -0700

    Updated model and implemented reset link generation

--- api/routers/users_router.py
+++ api/routers/users_router.py
@@ -15,3 +15,10 @@ async def create_user(request: Request, user_in: user_model.UserCreate) -> JSONR
     user_in = jsonable_encoder(user_in)
     new_user = await UserService.create(request, user_in)
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"messsage" : "User added successfully", "data" : new_user})
+
+@router.post("/reset", status_code=status.HTTP_202_ACCEPTED, response_description="Get reset password link")
+async def get_reset_link(request: Request, user_in: user_model.UserBase) -> JSONResponse:
+    user_in = jsonable_encoder(user_in)
+    user_to_reset = await UserService.get_user_by_email(request, user_in)
+    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, 
+    content={"messsage" : "Token generated successfully", "token" : user_to_reset['_id']})
--- api/services/user_service.py
+++ api/services/user_service.py
@@ -4,13 +4,22 @@ from ..crud.user_crud import CRUDUser
 from ..models import user_model
 
 class UserService():
-    async def get(request: Request, id: str) -> Optional[user_model.UserInDB]:
+    async def get_user_by_id(request: Request, id: str) -> Optional[user_model.UserInDB]:
         retrieved_user = await CRUDUser.get_by_id(request, id)
         if not retrieved_user:
             raise HTTPException(
-            status_code=404, 
-            detail=f"User with ID {id} not found"
-        )
+                status_code=404, 
+                detail=f"User with ID {id} not found"
+            )
+        return retrieved_user
+
+    async def get_user_by_email(request: Request, user_in: user_model.UserBase) -> Optional[user_model.UserInDB]:
+        retrieved_user = await CRUDUser.get_by_email(request, user_in)
+        if not retrieved_user:
+            raise HTTPException(
+                status_code=404,
+                detail=f"User with email {user_in['email']} not found"
+            )
         return retrieved_user
 
     async def create(request: Request, user_in: user_model.UserCreate) -> Optional[user_model.UserInDB]:

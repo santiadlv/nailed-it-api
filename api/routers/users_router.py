34fcad384b437539df39e0843ea25b9615925766
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-03-26 01:57:38 -0700

    Added Login Model and routes

--- api/routers/users_router.py
+++ api/routers/users_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from ..models import user_model
+from ..models import login_model
 from ..services.user_service import UserService
 
 router = APIRouter(
@@ -15,3 +16,9 @@ async def create_user(request: Request, user_in: user_model.UserCreate) -> JSONR
     user_in = jsonable_encoder(user_in)
     new_user = await UserService.create(request, user_in)
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"messsage" : "User added successfully", "data" : new_user})
+
+@router.post("/login", status_code=status.HTTP_200_OK, response_description="User Login", response_model=login_model.UserLogin)
+async def validate_user(request: Request, user_login: login_model.UserCheck) -> JSONResponse:
+    user_login = jsonable_encoder(user_login)
+    validate_password = await UserService.login(request, user_login)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"messsage" : "Login Succesful", "data" : validate_password})

4dc2f7fcbefaae0a208c471eda4f27e198ec1c24
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-05-18 23:49:00 -0600

    MA-41: Created new models to add and remove hours, added data validators, refactored CRUD operations

--- api/crud/hours_crud.py
+++ api/crud/hours_crud.py
@@ -1,15 +1,15 @@
 from typing import Optional
 from fastapi import Request
 from fastapi.encoders import jsonable_encoder
-from ..models import hours_model
+from ..models.hours_model import AvailabilityBase, AvailabilityGet
 from ..core import settings
 
 
 class CRUDHours():
-    async def insert(request: Request, obj_in: hours_model.AvailabilityBase) -> Optional[hours_model.AvailabilityBase]:
-        new_hours = hours_model.AvailabilityBase(
+    async def insert(request: Request, obj_in: AvailabilityBase) -> Optional[AvailabilityBase]:
+        new_hours = AvailabilityBase(
             service_id=obj_in['service_id'],
-            hours=obj_in['hours']
+            hours=sorted(obj_in['hours'], key=lambda i: i['timeStart'])
         )
 
         new_hours = new_hours.set_hours_id()
@@ -17,11 +17,15 @@ class CRUDHours():
         new_hours = await request.app.mongodb[settings.MONGODB_COLLECTION_HOURS].insert_one(serialized_hours)
         return serialized_hours
 
-    async def retrieve(request: Request, obj_in: hours_model.AvailabilityGet) -> Optional[hours_model.AvailabilityBase]:
+    async def retrieve(request: Request, obj_in: AvailabilityGet) -> Optional[AvailabilityBase]:
         hours = await request.app.mongodb[settings.MONGODB_COLLECTION_HOURS].find_one({'service_id': obj_in['service_id']})
         return hours
 
-    async def update(request: Request, obj_in: hours_model.AvailabilityBase) -> Optional[hours_model.AvailabilityBase]:
+    async def retrieve_by_id(request: Request, service_id: str) -> Optional[AvailabilityBase]:
+        hours = await request.app.mongodb[settings.MONGODB_COLLECTION_HOURS].find_one({'service_id': service_id})
+        return hours
+
+    async def update(request: Request, obj_in: AvailabilityBase) -> Optional[AvailabilityBase]:
         obj_in = obj_in.set_hours_id()
         query = {"service_id" : obj_in.service_id}
         update_values = { "$set" : {"hours" : obj_in.hours}}
--- api/models/hours_model.py
+++ api/models/hours_model.py
@@ -4,24 +4,35 @@ from bson.objectid import ObjectId
 from pydantic import BaseModel, Field, validator
 from .objectid_model import PyObjectId
 
+def check_field_not_empty(value: str) -> str:
+    assert value != "", 'Empty strings are not allowed'
+    return value
+
+def check_valid_hour(value: int) -> int:
+    assert value in range(0, 1440), f'Time value {str(value)} is not inside acceptable range (0 - 1440)'
+    return value
 
 class AvailabilityBase(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
     service_id: str = Field(...)
     hours: List[dict[str, Any]]
 
+    service_id_validate = validator('service_id', allow_reuse=True)(check_field_not_empty)
+
     @validator('hours')
-    def check_valid_hour(cls, v):
+    def check_valid_hour_in_array(cls, v):
         for hours in v:
             for key in hours:
-                if key != 'id':
-                    assert hours[key] in range(0, 1440), 'Time value is not inside acceptable range (0 - 1440)'
+                if key != 'hour_id':
+                    assert hours[key] in range(0, 1440), f'A time value in the array ({str(hours[key])}) \
+                                                           is not inside the acceptable range (0 - 1440)'
         return v
 
     def set_hours_id(self):
         for dict in self.hours:
-            new_uuid = str(uuid4())
-            dict.update(hour_id=new_uuid)
+            if 'hour_id' not in dict:
+                new_uuid = str(uuid4())
+                dict.update(hour_id=new_uuid)
         return self
 
     class Config:
@@ -40,11 +51,50 @@ class AvailabilityBase(BaseModel):
 class AvailabilityGet(BaseModel):
     service_id: str = Field(...)
 
+    service_id_validate = validator('service_id', allow_reuse=True)(check_field_not_empty)
+
     class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
         schema_extra = {
             "example": {
                 "service_id": "60860830041aa13d76554242",
+            }
+        }
+
+class AvailabilityAdd(BaseModel):
+    service_id: str = Field(...)
+    timeStart: int = Field(...)
+    timeEnd: int =  Field(...)
+
+    service_id_validate = validator('service_id', allow_reuse=True)(check_field_not_empty)
+    time_start_validate = validator('timeStart', allow_reuse=True)(check_valid_hour)
+    time_end_validate = validator('timeEnd', allow_reuse=True)(check_valid_hour)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+                "service_id": "60860830041aa13d76554242",
+                "timeStart": 480,
+                "timeEnd": 510
+            }
+        }
+
+class AvailabilityRemove(BaseModel):
+    service_id: str = Field(...)
+    hour_id: str = Field(...)
+    
+    service_id_validate = validator('service_id', allow_reuse=True)(check_field_not_empty)
+    hour_id_validate = validator('hour_id', allow_reuse=True)(check_field_not_empty)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+                "service_id": "60860830041aa13d76554242",
+                "hour_id": "395c7e3a-9c71-4e03-8d1c-c424f51716c7"
             }
         }

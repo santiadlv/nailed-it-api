bba4fc1581b090a42a563187bf7270686f5bfc36
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-05-19 19:59:21 -0600

    Remove Available Hour, when reservation is created

--- api/crud/reservation_crud.py
+++ api/crud/reservation_crud.py
@@ -1,8 +1,10 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import Request
 from fastapi.encoders import jsonable_encoder
 from ..models import reservation_model
 from ..core import settings
+from ..routers import hours_router, users_router
+import datetime
 
 class CRUDReservation():
     async def create(request: Request, obj_in: reservation_model.ReservationBase) -> Optional[reservation_model.ReservationBase]:
@@ -15,4 +17,40 @@ class CRUDReservation():
         )
         serialized_reservation = jsonable_encoder(new_reservation)
         new_reservation = await request.app.mongodb[settings.MONGODB_COLLECTION_RESERVATIONS].insert_one(serialized_reservation)
+        info_reservation = await CRUDReservation.informationReservation(request, obj_in)
+        return info_reservation
+
+    async def informationReservation(request: Request, obj_in: reservation_model.ReservationInfo) -> Optional[reservation_model.ReservationInfo]:
+        dateToday = datetime.datetime.now()
+        service_info = await request.app.mongodb[settings.MONGODB_COLLECTION_SERVICES].find_one({'_id': obj_in['service_id']})
+        salon_name = await request.app.mongodb[settings.MONGODB_COLLECTION_SALONS].find_one({'_id': service_info["salon_id"]})
+        reservation_info = reservation_model.ReservationInfo(
+            user_id=obj_in['user_id'],
+            service_id=obj_in['service_id'],
+            time_start=obj_in['time_start'],
+            time_end=obj_in['time_end'],
+            hour_id=obj_in['hour_id'],
+            salon_name=salon_name["name"],
+            service_name=service_info["name"],
+            date=dateToday,
+            price=service_info["price"]
+        )
+        serialized_reservation = jsonable_encoder(reservation_info)
         return serialized_reservation
+
+    async def removeAvailability(request: Request, obj_in: reservation_model.ReservationConfirm) -> Optional[reservation_model.ReservationConfirm]:
+        reservation_confirm = reservation_model.ReservationConfirm(
+            service_id=obj_in['service_id'],
+            hour_id=obj_in['hour_id']
+        )
+        serialized_reservation = jsonable_encoder(reservation_confirm)
+        reservation_confirm = await hours_router.remove_service_hour(request, serialized_reservation)
+
+    async def get_reservations(request: Request, user_id: str) -> Optional[List[reservation_model.ReservationBase]]:
+        reservations = await request.app.mongodb[settings.MONGODB_COLLECTION_RESERVATIONS].find({"user_id": user_id}).to_list(1000)
+        return reservations
+
+    async def get_reservation_by_id(request: Request, user_id: str, reservation_id: str) -> Optional[reservation_model.ReservationBase]:
+        if (reservation := await request.app.mongodb[settings.MONGODB_COLLECTION_RESERVATIONS].find_one({'_id': reservation_id, 'user_id': user_id})) is not None:
+            return reservation
+        else: return None
--- api/models/reservation_model.py
+++ api/models/reservation_model.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from bson.objectid import ObjectId
 from pydantic import BaseModel, Field
 from .objectid_model import PyObjectId
@@ -24,3 +25,45 @@ class ReservationBase(BaseModel):
             }
         }
         
+class ReservationConfirm(BaseModel):
+    service_id: str = Field(...)
+    hour_id: str  = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+              "service_id": "60860830041aa13d76554242",
+              "hour_id": "d7b9599a-28da-48c7-b6e6-e6eba7a502fa"
+            }
+        }
+
+class ReservationInfo(BaseModel):
+    user_id: str = Field(...)
+    service_id: str = Field(...)
+    time_start: int = Field(...)
+    time_end: int = Field(...)
+    hour_id: str  = Field(...)
+    salon_name: str = Field(...)
+    service_name: str = Field(...)
+    date: datetime = Field(...)
+    price: str = Field(...)
+
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+              "user_id": "605cd9ca1ec4bf19fe3f9581",
+              "service_id": "60860830041aa13d76554242",
+              "time_start": 870,
+              "time_end": 900,
+              "hour_id": "d7b9599a-28da-48c7-b6e6-e6eba7a502fa",
+              "salon_name": "Beautify Salon",
+              "service_name": "Nail Cleaning and Shellac",
+              "date": "60860830041aa13d76554242",
+              "price": "200.00"
+            }
+        }

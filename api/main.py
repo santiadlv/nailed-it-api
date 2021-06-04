c1dfef63b12aa1a8ca41b59ecfc7ba2ffc5616e8
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-06-04 02:23:47 -0600

    MA-43: Added files for reviews

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 from .core import settings
-from .routers import users_router, salons_router, services_router, reservation_router, hours_router
+from .routers import users_router, salons_router, services_router, reservation_router, hours_router, reviews_router
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
@@ -32,3 +32,4 @@ app.include_router(router=salons_router.router)
 app.include_router(router=services_router.router)
 app.include_router(router=reservation_router.router)
 app.include_router(router=hours_router.router)
+app.include_router(router=reviews_router.router)

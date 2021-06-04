b0b1bd6ee4f5db8116a7060cd1f00ee31d9e3764
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-06-03 20:26:57 -0600

    MA-44: Update service model, create new categories model and create CRUD with filtering

--- api/models/service_model.py
+++ api/models/service_model.py
@@ -1,6 +1,8 @@
 from bson.objectid import ObjectId
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from .objectid_model import PyObjectId
+
+service_categories_list = ["Nails", "Haircutting, coloring and styling", "Tanning", "Massages", "Hair removal", "Makeup", "Skin care", "Cosmetic beauty", "Complementary care"]
 
 class ServiceBase(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
@@ -11,6 +13,7 @@ class ServiceBase(BaseModel):
     price: str = Field(...)
     rating: str = Field(...)
     description: str = Field(...)
+    category: str = Field(...)
     salon_id: str = Field(...)
 
     class Config:
@@ -26,6 +29,7 @@ class ServiceBase(BaseModel):
                 "price": "200.00",
                 "rating": "4.1",
                 "description": "A shellac manicure is the perfect version of a regular manicure.  It is a dry manicure that includes all of the basics of a regular manicure.   Your nails are cleaned up, the skin around your nails is pampered, and last of all a polish is applied to the nail with exactness.  However, the process of application is uniquely different. Instead of applying a nail polish that is bound to chip, scratch, or peel, a Shellac manicure is a polish/gel product that when cured under a UV lamp hardens and lasts.",
+                "category": "Nail treatments",
                 "salon_id": "6085df4104159a9c9c81b968"
             }
         }
@@ -42,4 +46,22 @@ class ServiceIdentifier(BaseModel):
                 "id": "608608d7041aa13d76554244"
             }
         }
-        
+
+class ServiceCategory(BaseModel):
+    categories: list = Field(...)
+
+    @validator('categories')
+    def check_valid_category(cls, v):
+        for category in v:
+            assert category in service_categories_list, f"Category '{category}' does not exist"
+        return v
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "categories": service_categories_list
+            }
+        }
